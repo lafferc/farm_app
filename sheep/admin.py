4135b5c71b7edcaee200bdb8860df219b4de817f
lafferc <lafferc@tcd.ie>
2016-12-03 14:53:22 +0000

    added sheep to admin GUI

--- sheep/admin.py
+++ sheep/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from sheep.models import Dose, Sheep, DoseGiven, Weight, LambingYear
 
-# Register your models here.
+
+class DoseGivenInLine(admin.TabularInline):
+    model = DoseGiven
+    extra = 0
+
+
+class WeightInLine(admin.TabularInline):
+    model = Weight
+    extra = 0
+
+
+class LambingYearInline(admin.TabularInline):
+    model = LambingYear
+    extra = 0
+
+ 
+class SheepAdmin(admin.ModelAdmin):
+    inlines = ( DoseGivenInLine, WeightInLine, LambingYearInline)
+
+
+admin.site.register(Dose)
+admin.site.register(Sheep, SheepAdmin)
+admin.site.register(Weight)
+admin.site.register(LambingYear)
+
