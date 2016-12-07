33a80294dff8102fc830e4f4eca35a999acf2c39
lafferc <lafferc@tcd.ie>
2016-12-07 00:27:05 +0000

    added email settings

--- test_app/settings.py
+++ test_app/settings.py
@@ -117,3 +117,10 @@ LOGGING = {
         },
     },
 }
+
+#email settings
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ['DJANGO_EMAIL_USER']
+EMAIL_HOST_PASSWORD = os.environ['DJANGO_EMAIL_PASS']
