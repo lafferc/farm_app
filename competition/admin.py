a7508292f7d77384d3926e2afbf9f6ce03349e0f
lafferc <lafferc@tcd.ie>
2016-12-10 22:49:40 +0000

    added other admin user actions to dashboard for superusers

--- competition/admin.py
+++ competition/admin.py
@@ -8,6 +8,13 @@ g_logger = logging.getLogger(__name__)
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'code')
+    list_filter = (
+        ('sport', admin.RelatedOnlyFieldListFilter),
+    )
+    def get_readonly_fields(self, request, obj):
+        if obj:
+            return ('sport',)
+        return ()
 
 
 class ParticipantInline(admin.TabularInline):
@@ -20,7 +27,7 @@ class ParticipantInline(admin.TabularInline):
 
 
 def pop_leaderboard(modeladmin, request, queryset):
-    g_logger.info("pop_leaderboard(%r, %r, %r", modeladmin, request, queryset)
+    g_logger.debug("pop_leaderboard(%r, %r, %r", modeladmin, request, queryset)
     for tournament in queryset:
         tournament.update_table()
 
@@ -29,6 +36,23 @@ class TournamentAdmin(admin.ModelAdmin):
     list_display = ('name',)
     inlines = ( ParticipantInline, )
     actions = [pop_leaderboard]
+    list_filter = (
+        ('sport', admin.RelatedOnlyFieldListFilter),
+        "state",
+    )
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'sport', 'state', 'bonus', 'late_get_bonus', 'winner', 'add_matches')
+        }),
+    )
+
+    def get_readonly_fields(self, request, obj):
+        if obj:
+            return ('sport', 'bonus', 'late_get_bonus', 'winner')
+        return ('winner')
+
+    def get_fieldsets(self, request, obj):
+        return ((None, {'fields': ('name', 'sport')}),)
 
 
 def calc_match_result(modeladmin, request, queryset):
@@ -46,14 +70,30 @@ def calc_match_result(modeladmin, request, queryset):
 
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('match_id', 'home_team', 'away_team', 'kick_off', 'score')
+    list_filter = (
+        ('tournament', admin.RelatedOnlyFieldListFilter),
+    )
     actions = [calc_match_result]
+    fieldsets = (
+        (None, {
+            'fields': ('tournament', 'match_id', 'home_team', 'away_team', 'kick_off', 'score')
+        }),
+    )
+    def get_readonly_fields(self, request, obj):
+        if obj:
+            return ('tournament', 'match_id', 'home_team', 'away_team')
+        return ('score',)
 
 
 class PredictionAdmin(admin.ModelAdmin):
     list_display = ('user', 'match', 'entered')
     readonly_fields = ('score', "late")
 
-    list_filter = ('match','user')
+    list_filter = (
+        ('match', admin.RelatedOnlyFieldListFilter),
+        ('user', admin.RelatedOnlyFieldListFilter),
+        'match__tournament',
+    )
 
 
 admin.site.register(Sport)
